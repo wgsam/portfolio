f319404bbc45964a69d691a6e26529edb09fa027
wgsam <wgsam666@gmail.com>
2021-12-11 01:26:55 +0800

    commit all 1st time

---
+++ module/project/meta-reward/js/app.js
@@ -0,0 +1,35 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal('.meta-logo', {
+    delay: 200,
+    duration: 500,
+    origin: 'top',
+    distance: '100px',
+    easing: 'ease-in-out'
+});
+
+sr.reveal('.meta-download-textfield', {
+    delay: 200,
+    duration: 500,
+    origin: 'bottom',
+    distance: '100px',
+    easing: 'ease-in-out'
+});
+
+sr.reveal('.meta-download-btnfield', {
+    delay: 200,
+    duration: 600,
+    origin: 'bottom',
+    distance: '200px',
+    easing: 'ease-in-out'
+});
+
+
+sr.reveal('.bg-phone', {
+    delay: 400,
+    duration: 400,
+    origin: 'right',
+    distance: '200px',
+    easing: 'ease-in-out'
+});
